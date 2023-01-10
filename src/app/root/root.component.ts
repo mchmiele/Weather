876a5dfad03c807e7d4193f134f7edd04c407387
Mateusz Chmielewski <mchmielewski@vezto.pl>
2023-01-10 22:30:23 +0100

    Reordering folders and renaming AppModule/AppComponent to RootModule/RootComponent

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -5,10 +5,10 @@ import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.scss']
 })
-export class AppComponent {
+export class RootComponent {
 
     constructor() {
 
--- src/app/root/root.module.ts
+++ src/app/root/root.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { ZipcodeEntryComponent } from './zipcode-entry/zipcode-entry.component';
-import {LocationService} from "./location.service";
-import { ForecastsListComponent } from './forecasts-list/forecasts-list.component';
-import {WeatherService} from "./weather.service";
-import { CurrentConditionsComponent } from './current-conditions/current-conditions.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { RootComponent } from './root.component';
+import { ZipcodeEntryComponent } from './../zipcode-entry/zipcode-entry.component';
+import {LocationService} from "../services/location.service";
+import { ForecastsListComponent } from './../forecasts-list/forecasts-list.component';
+import {WeatherService} from "./../services/weather.service";
+import { CurrentConditionsComponent } from './../current-conditions/current-conditions.component';
+import { MainPageComponent } from './../main-page/main-page.component';
 import {RouterModule} from "@angular/router";
-import {routing} from "./app.routing";
+import {routing} from "./root.routing";
 import {HttpClientModule} from "@angular/common/http";
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    RootComponent,
     ZipcodeEntryComponent,
     ForecastsListComponent,
     CurrentConditionsComponent,
@@ -32,6 +32,6 @@ import { environment } from '../environments/environment';
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [LocationService, WeatherService],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
-export class AppModule { }
+export class RootModule { }
