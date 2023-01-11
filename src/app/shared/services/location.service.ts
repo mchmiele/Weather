0b4ac696a5abbd7951822db9d66ad6659af9abfe
Mateusz Chmielewski <mchmielewski@vezto.pl>
2023-01-11 13:53:50 +0100

    Disabling working button

--- src/app/shared/services/location.service.ts
+++ src/app/shared/services/location.service.ts
@@ -21,10 +21,11 @@ export class LocationService {
     }
 
     addLocation(location: Location) {
-        this.locations.push(location);
-        localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-        setTimeout(()=>{ this.locationProcess.next('done'); }, 500) // timeout on purpose to demonstrate the middle-state: working
-        
+        setTimeout(() => {
+            this.locations.push(location);
+            localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
+            this.locationProcess.next('done');
+        }, 500) // timeout on purpose to demonstrate the middle-state: working
     }
 
     removeLocation(zipCode: string) {
